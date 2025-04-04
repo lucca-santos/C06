a40fef45b212186c47515250abb24484251d85f6
lucca santos <lucca.santos@get.inatel.br>
2025-04-04 17:16:30 -0300

    Movendo para pasta.

---
+++ PCMANIA/src/HardwareBasico.java
@@ -0,0 +1,9 @@
+public class HardwareBasico {
+    String nome;
+    int capacidade;
+
+    public HardwareBasico(String nome, int capacidade) {
+        this.nome = nome;
+        this.capacidade = capacidade;
+    }
+}
