9bb58306b1def689ca970fdbd2b7ca4cb1658bbb
lucca santos <lucca.santos@get.inatel.br>
2025-04-04 17:16:01 -0300

    Projeto PC MANIA.

--- src/HardwareBasico.java
+++
@@ -1,9 +0,0 @@
-public class HardwareBasico {
-    String nome;
-    int capacidade;
-
-    public HardwareBasico(String nome, int capacidade) {
-        this.nome = nome;
-        this.capacidade = capacidade;
-    }
-}
